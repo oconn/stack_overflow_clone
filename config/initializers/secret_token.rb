7a61f3b0687e22cbe96d27584c9533258281a529
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-12 10:33:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StackOflow::Application.config.secret_key_base = '5ca5698be37d5f1bccfc8b1d2eac059bd2c6ceb5330009a64632f27fab251bd1521a033f014dead86012672fc5b0b961ee1036fc6ab8ea06c5a5562d84b06853'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StackOflow::Application.config.session_store :cookie_store, key: '_stack_oflow_session'
