df984edfa55eafdf8d4622af33876f174ad66806
= <=>
2014-03-12 13:37:22 -0500

    Nav bar finished for now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,4 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
-//= require_tree .r
+//= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def current_user
-    User.where(id: session[:user_id]).first
-  end
 end
