0636ab1f618ae8dcc9c3087a97ece80f0b9ec6b3
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-12 16:52:11 -0500

    add username to devise controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :questions
+
+  validates :username, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140312205321) do
-  
+ActiveRecord::Schema.define(version: 20140312210726) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,13 +23,15 @@ ActiveRecord::Schema.define(version: 20140312205321) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-  
+
   create_table "comments", force: true do |t|
     t.integer  "user_id"
     t.integer  "commentable_id"
     t.string   "commentable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-  
+
   create_table "questions", force: true do |t|
     t.integer  "user_id"
     t.string   "title"
