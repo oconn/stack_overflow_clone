ccf0e9f182f689942da76481f9c0d37d5d04279c
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-12 15:27:50 -0500

    user can browser questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+25.times do
+	User.create(username: Faker::Lorem.word,
+		          email: Faker::Internet.email,
+		          password: "12345678",
+		          password_confirmation: "12345678")
+end
+
+User.all.each do |user|
+  2.times do 
+    Question.create(user_id: user.id,
+                    title: Faker::Lorem.words(num = 4).join,
+                    body: Faker::Lorem.paragraph) 
+  end
+end
