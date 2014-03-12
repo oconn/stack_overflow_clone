b2e1072f5c3c85629564ae8598a55adec389249e
= <=>
2014-03-12 15:49:48 -0500

    added test for asking question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ActiveRecord::Base
   belongs_to :user
 
+  validates :title, presence: true
+  validates :body, presence: true
+  validates_length_of :title, :minimum => 5, :allow_blank => false
+  validates_length_of :body, :minimum => 5, :allow_blank => false
 end
