b89621ab8c810af92078c13e6920cab1097c1c34
dan <dandydanny@gmail.com>
2014-03-13 17:32:39 -0500

    readded comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :questions
   has_many :votes
-  
+  has_many :comments
+
   validates :username, presence: true, uniqueness: true
 end
