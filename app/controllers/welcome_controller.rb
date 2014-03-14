f4d10f277db65352bf143de586c8e75e7dd9df6f
dan <dandydanny@gmail.com>
2014-03-14 15:39:12 -0500

    added top users page and added user profiles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,12 @@ class WelcomeController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def top_users
+    @users = User.all.order('vote_points DESC').limit(10)
+  end
+
+  def user
+    @user = User.find(params[:format])
+  end
 end
