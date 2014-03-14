6c51161441dec405878990dca4d4b54f1deddbf3
Neal Peters <nealcpeters@gmail.com>
2014-03-14 13:31:31 -0500

    Added tests for validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,16 +5,13 @@ class Question < ActiveRecord::Base
   has_many :votes, as: :voteable
   has_one :visit, as: :visitable
 
-  validates :title, presence: true
-  validates :body, presence: true
   validates :user_id, presence: true
-  validates_length_of :title, :minimum => 5, :allow_blank => false
-  validates_length_of :body, :minimum => 5, :allow_blank => false
+  validates_length_of :title, :minimum => 5
+  validates_length_of :body, :minimum => 5
 
   # scope :popular,
   #   select("questions.title, count(visits.unique_visits) as visits_count").
   #   joins(:visits).
   #   group("questions.title").
   #   order("visits_count DESC").
-
 end
